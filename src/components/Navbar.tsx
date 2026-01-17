91ea8cc9c47c51e544cbb841d2c6bb903241b67a
DasunThathsara <dasun.thathsara.sri@gmail.com>
2026-01-17 20:03:52 +0530

    initialize the project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className=''>
+        <nav className='bg-gray-800 text-white p-4'>
+            <div className='container mx-auto'>
+                <h1 className='text-xl font-bold'>CoMind</h1>
+            </div>
+        </nav>
+    </div>
+  )
+}
