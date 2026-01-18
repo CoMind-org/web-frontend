38be2849a5fe034f1bd923e8d0a6cc55867f5cb5
DasunThathsara <dasun.thathsara.sri@gmail.com>
2026-01-18 17:12:15 +0530

    feat: project add card

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,7 +6,7 @@ import ProjectCard from "../components/ProjectCard"
 
 export default function Dashboard() {
   return (
-    <div className="bg-[#020018] w-screen h-screen">
+    <div className="bg-[#020018] w-screen h-screen overflow-x-hidden">
       <Navbar />
       <Sidebar />
       <div className='md:ml-64 p-10'>
@@ -40,6 +40,18 @@ export default function Dashboard() {
             description="Product catalog and cart service"
             lastActive="Last active 2 hours ago"
           />
+
+          <div className="group w-90 h-55 bg-[#010022] rounded-lg border border-dashed border-blue-500/20 px-6 py-5 hover:border-blue-500/50 cursor-pointer transition-all duration-300 hover:-translate-y-1">
+            <div className="h-full flex items-center justify-center font-medium text-gray-500 group-hover:text-blue-400">
+              <div className="flex flex-col items-center justify-center gap-2">
+                <div className="bg-gray-500/30 p-4 rounded-full">
+                  <Plus size={18} />
+                </div>
+                <p>Create New Backend</p>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
